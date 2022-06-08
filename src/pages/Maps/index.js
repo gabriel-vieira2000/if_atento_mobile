da97bbe54a410d0e258422055cb64e79de8c849f
Gabriel Vieira Cardoso <gvc20002016@gmail.com>
2022-06-07 21:03:05 -0300

    Adicionando Funcionalidade aos Poligonos dos Setores

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import MapView, {Marker, Polygon} from "react-native-maps";
 
-import { Layout, BottomNavigation , BottomNavigationTab} from "@ui-kitten/components";
+import { Layout, BottomNavigation , BottomNavigationTab, Button} from "@ui-kitten/components";
 
 export default function Maps() {
-
+  const [nomeSetor, setNomeSetor] = useState("");
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} mapType="hybrid" minZoomLevel={15} maxZoomLevel={20} zoomTapEnabled={false} zoomControlEnabled={false}
+      <MapView style={styles.map} mapType="hybrid" minZoomLevel={15} maxZoomLevel={20} zoomTapEnabled={false} zoomControlEnabled={false} 
         initialRegion={{
           latitude: -21.350371,
           longitude: -46.528377,
@@ -21,29 +21,38 @@ export default function Maps() {
             {name: "predioInfo2", latitude: -21.350605, longitude: -46.526832},
             {name: "predioInfo3", latitude: -21.350980, longitude: -46.526956},
             {name: "predioInfo4", latitude: -21.350854, longitude: -46.527341},
-          ]} strokeWidth={2} strokeColor="#000" fillColor="rgba(0,0,0,0.3)" tappable={true}
+          ]} strokeWidth={2} strokeColor="#000" fillColor="rgba(0,0,0,0.3)" onPress={() => {setNomeSetor("Prédio de Informática")}} tappable={true}
           />
           <Polygon coordinates={[
             {name: "refeitorio1", latitude: -21.350741, longitude: -46.527749},
             {name: "refeitorio2", latitude: -21.350576, longitude: -46.527698},
             {name: "refeitorio3", latitude: -21.350706, longitude: -46.527337},
             {name: "refeitorio4", latitude: -21.350843, longitude: -46.527359},
-          ]} strokeWidth={2} strokeColor="#fff" fillColor="rgba(255,255,255,0.3)" tappable={true}
+          ]} strokeWidth={2} strokeColor="#fff" fillColor="rgba(255,255,255,0.3)" onPress={() => {setNomeSetor("Refeitório")}} tappable={true}
           />
           <Polygon coordinates={[
             {name: "refeitorio1", latitude: -21.350247, longitude: -46.527686},
             {name: "refeitorio2", latitude: -21.350259, longitude: -46.527797},
             {name: "refeitorio3", latitude: -21.350030, longitude: -46.527855},
             {name: "refeitorio4", latitude: -21.350006, longitude: -46.527753},
-          ]} strokeWidth={2} strokeColor="rgba(255,0,0,1)" fillColor="rgba(255,0,0,0.3)" tappable={true}
+          ]} strokeWidth={2} strokeColor="rgba(255,0,0,1)" fillColor="rgba(255,0,0,0.3)" onPress={() => {setNomeSetor("Cooperativa")}} tappable={true} 
           />
         </MapView>
+        
+        <SetorSelecionado nomeSetor={nomeSetor}></SetorSelecionado>
 
       <Layout style={styles.footer} level="1">
         <Text>Menu Inferior</Text>
       </Layout>
     </View>
   );
+}
+
+function SetorSelecionado(props){
+  return (
+    <Text style={styles.setor_selecionado}>{props.nomeSetor}</Text>
+  )
+
 }
 const styles = StyleSheet.create({
   container: {
@@ -55,6 +64,13 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  setor_selecionado : {
+    position: "absolute",
+    bottom: 100,
+    backgroundColor: "red",
+    color: "white",
+    zIndex: 2
   },
   footer: {
     bottom: 0,
